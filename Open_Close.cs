c2efa5c7d593eeab104fea4c86ca8ee8d4241bca
baloxegal <baloxegal@gmail.com>
2021-03-08 23:24:30 +0200

    Add project files.

---
+++ Open_Close.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace OpenClosePrinciple
+{
+    //Violating Open-Closed Principle + Violating Single Responsibility Principle
+    abstract class Vehicle
+    {
+        int passangers;
+        int cargo;
+        public int Passangers
+        {
+            get
+            {
+                return passangers;
+            }
+            set
+            {
+                if (value >= 0)
+                    passangers = value;
+            }
+        }
+        public int Cargo
+        {
+            get
+            {
+                return cargo;
+            }
+            set
+            {
+                if (value >= 0)
+                    cargo = value;
+            }
+        }
+    }
+    class PassangersVehicle : Vehicle
+    {
+        public PassangersVehicle(int passangers)
+        {
+            this.Passangers = passangers;
+        }        
+    }
+    class CargoVehicle : Vehicle
+    {
+        public CargoVehicle(int cargo)
+        {
+            this.Cargo = cargo;
+        }           
+    }
+    class Rout
+    { 
+        public static void WhatRout(Vehicle vehicle)
+        {
+            if(vehicle is PassangersVehicle)
+                Console.WriteLine("Vehicle rout to Gara de Nord");
+            else if(vehicle is CargoVehicle)
+                Console.WriteLine("Vehicle rout to str. Uzinilor");           
+        }
+    }
+
+    //Compliance with the Open-Closed Principle + Compliance with the Single Responsibility Principle
+    abstract class CorrectVehicle
+    {
+        public abstract void CorrectRout();
+    }
+    class CorrectPassangersVehicle : CorrectVehicle
+    {
+        int correctPassangers;
+        public CorrectPassangersVehicle(int correctPassangers)
+        {
+            this.correctPassangers = correctPassangers;
+        }
+        public int CorrectPassangers
+        {
+            get
+            {
+                return correctPassangers;
+            }
+            set
+            {
+                if (value >= 0)
+                    correctPassangers = value;
+            }
+        }
+        public override void CorrectRout()
+        {
+            Console.WriteLine("Vehicle rout to Gara de Sud");
+        }
+
+    }
+    class CorrectCargoVehicle : CorrectVehicle
+    {
+        int correctCargo;
+        public CorrectCargoVehicle(int correctCargo)
+        {
+            this.correctCargo = correctCargo;
+        }
+        public int CorrectCargo
+        {
+            get
+            {
+                return correctCargo;
+            }
+            set
+            {
+                if (value >= 0)
+                    correctCargo = value;
+            }
+        }
+        public override void CorrectRout()
+        {
+            Console.WriteLine("Vehicle rout to str. Muncesti");
+        }
+
+    }
+    class CorrectClassRout
+    {
+        public static void ThisIsCorrectRout(CorrectVehicle correctVehicle)
+        {
+            correctVehicle.CorrectRout();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Exception_Handling_And_Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Violating Open-Closed Principle + Violating Single Responsibility Principle
+            OpenClosePrinciple
+            OpenVehicle passangersVehicle = new PassangersVehicle(40);
+            Vehicle cargoVehicle = new CargoVehicle(5000);
+            Rout.WhatRout(passangersVehicle);
+            Rout.WhatRout(cargoVehicle);
+            Console.WriteLine();
+
+            //Compliance with the Open-Closed Principle + Compliance with the Single Responsibility Principle
+            CorrectVehicle correctPassangersVehicle = new CorrectPassangersVehicle(60);
+            CorrectVehicle correctCargoVehicle = new CorrectCargoVehicle(10000);
+            CorrectClassRout.ThisIsCorrectRout(correctPassangersVehicle);
+            CorrectClassRout.ThisIsCorrectRout(correctCargoVehicle);
+            Console.WriteLine();
+
+            //Violating Liscov Substitution Principle
+            NotLiscovPassangersVehicle notLiscovPassangersVehicle = new NotLiscovPassangersVehicle(100);
+            NotLiscovCargoVehicle notLiscovCargoVehicle = new NotLiscovCargoVehicle(15000);
+            NotLiscovRout.NotLiscovPassangersRout(notLiscovPassangersVehicle);
+            NotLiscovRout.NotLiscovCargoRout(notLiscovCargoVehicle);
+            Console.WriteLine();
+
+            //Compliance with the Liscov Substitution Principle
+            LiscovVehicle liscovPassangersVehicle = new LiscovPassangersVehicle(10);
+            LiscovVehicle liscovCargoVehicle = new LiscovCargoVehicle(20000);
+            LiscovRout.ThisIsLiscovRout(liscovPassangersVehicle);
+            LiscovRout.ThisIsLiscovRout(liscovCargoVehicle);
+            Console.WriteLine();
+
+            //Violating Interface Substitution Principle
+            NotInterfaceSegregatedVehicle notInterfaceSegregatedPassangersVehicle = new NotInterfaceSegregatedPassangersVehicle(10);
+            NotInterfaceSegregatedVehicle notInterfaceSegregatedCargoVehicle = new NotInterfaceSegregatedCargoVehicle(3500);
+            NotInterfaceSegregatedRout.ThisIsNotInterfaceSegregatedRout(notInterfaceSegregatedPassangersVehicle);
+            NotInterfaceSegregatedRout.ThisIsNotInterfaceSegregatedRout(notInterfaceSegregatedCargoVehicle);
+            Console.WriteLine();
+
+            //Compliance with the Interface Substitution Principle
+            InterfaceSegregatedVehicle interfaceSegregatedPassangersVehicle = new InterfaceSegregatedPassangersVehicle(12);
+            InterfaceSegregatedVehicle interfaceSegregatedCargoVehicle = new InterfaceSegregatedCargoVehicle(6000);
+            InterfaceSegregatedRout.ThisIsInterfaceSegregatedRout(interfaceSegregatedPassangersVehicle);
+            InterfaceSegregatedRout.ThisIsInterfaceSegregatedRout(interfaceSegregatedCargoVehicle);
+        }
+    }
+}
